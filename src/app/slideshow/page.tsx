fea73f8bf34672addb43e5fbda437ca82246577e
Philip H <philiphubenette@gmail.com>
2023-03-26 22:15:14 +0200

    feat: create slideshow page

---
+++ src/app/slideshow/page.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import style from "./slideshow.module.css";
+import Image from "next/image";
+
+const page = () => {
+	return (
+		<div className={style.slideshow}>
+			<h1>Mitt liv i bilder</h1>
+			<div className={style.slideshowImageContainer}>
+				<Image
+					className={style.slideshowImage}
+					alt="Härjedalen fjällen"
+					src={"/fettjeafallet.jpg"}
+					width={321.5}
+					height={237}
+				/>
+				<Image
+					className={style.slideshowImage}
+					alt="Härjedalen fjällen"
+					src={"/fettjeafallet.jpg"}
+					width={321.5}
+					height={237}
+				/>
+				<Image
+					className={style.slideshowImage}
+					alt="Härjedalen fjällen"
+					src={"/fettjeafallet.jpg"}
+					width={321.5}
+					height={237}
+				/>
+				<Image
+					className={style.slideshowImage}
+					alt="Härjedalen fjällen"
+					src={"/fettjeafallet.jpg"}
+					width={321.5}
+					height={237}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default page;
