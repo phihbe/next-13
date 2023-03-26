d9e9dc8fcce3c27cc86c28c8251f6769a03812df
Philip H <philiphubenette@gmail.com>
2023-03-27 00:23:57 +0200

    feat: create contact us page

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./contact-us.module.css";
+
+const page = () => {
+	return (
+		<div className={style.contactUs}>
+			<h1>Contact us</h1>
+		</div>
+	);
+};
+
+export default page;
